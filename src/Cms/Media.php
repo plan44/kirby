bc43b4020ca12365b003b42c7c69e1fff8649c6f
Lukas Bestle <lukas@getkirby.com>
2020-06-17 21:23:10 +0200

    createMedia and protect options for file blueprint
    https://github.com/getkirby/ideas/issues/554

--- src/Cms/Media.php
+++ src/Cms/Media.php
@@ -3,6 +3,7 @@
 namespace Kirby\Cms;
 
 use Kirby\Data\Data;
+use Kirby\Http\Route;
 use Kirby\Toolkit\Dir;
 use Kirby\Toolkit\F;
 use Throwable;
@@ -46,8 +47,27 @@ class Media
                 return Response::redirect($file->mediaUrl(), 307);
             }
 
+            // check for user permissions
+            if (
+                $file->blueprint()->protect() === true &&
+                $file->permissions()->can('read') !== true
+            ) {
+                // handle this request like the file doesn't exist
+                Route::next();
+            }
+
             // send the file to the browser
-            return Response::file($file->publish()->mediaRoot());
+            if ($file->blueprint()->createMedia() === true) {
+                return Response::file($file->publish()->mediaRoot());
+            } elseif ($file->blueprint()->protect() === true) {
+                return Response::file($file->root(), [
+                    'headers' => [
+                        'Cache-Control' => 'no-cache, no-store, must-revalidate'
+                    ]
+                ]);
+            } else {
+                return Response::file($file->root());
+            }
         }
 
         // try to generate a thumb for the file
